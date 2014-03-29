9323705a04ac8e81fcd1c51c40973ebcf6ac2134
deathcap <deathcap@gmx.co.uk>
2014-03-29 15:51:08 -0700

    Add WASD controls

--- index.js
+++ index.js
@@ -3,11 +3,26 @@
 var createBasicCamera = require('basic-camera');
 
 var attachCamera = function(shell) {
-  var camera = createBasicCamera([45, -14, 11]); //[80, 16-10, 16]);
+  var camera = createBasicCamera([-22, -14, -48]); 
 
-  camera.rotateY(240 * Math.PI/180);
+  shell.bind('move-left', 'left', 'A');
+  shell.bind('move-right', 'right', 'D');
+  shell.bind('move-forward', 'forward', 'W');
+  shell.bind('move-back', 'back', 'S');
 
   shell.on('tick', function() {
+    if (shell.wasDown('move-left')) {
+      camera.position[0] += 1;
+    }
+    if (shell.wasDown('move-right')) {
+      camera.position[0] -= 1;
+    }
+    if (shell.wasDown('move-forward')) {
+      camera.position[2] += 1;
+    }
+    if (shell.wasDown('move-back')) {
+      camera.position[2] -= 1;
+    }
     //r += 1;
     //camera.rotateY(1 * Math.PI/180);
     //console.log(r);
@@ -16,6 +31,7 @@ var attachCamera = function(shell) {
   camera.lookAt = function(eye, center, up) { console.log(eye, center, up); }; // TODO: add to basic-camera, as in orbit-camera
 
   window.camera = camera;
+  window.shell = shell;
 
   return camera;
 };
