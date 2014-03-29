1e9e2576bb356d70f6c9e4009a9e659bb8340487
deathcap <deathcap@gmx.co.uk>
2014-03-29 15:53:46 -0700

    Add space/shift to ascend/descend

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var attachCamera = function(shell) {
   shell.bind('move-right', 'right', 'D');
   shell.bind('move-forward', 'forward', 'W');
   shell.bind('move-back', 'back', 'S');
+  shell.bind('move-up', 'up', 'space');
+  shell.bind('move-down', 'down', 'shift');
 
   shell.on('tick', function() {
     if (shell.wasDown('move-left')) {
@@ -17,15 +19,18 @@ var attachCamera = function(shell) {
     if (shell.wasDown('move-right')) {
       camera.position[0] -= 1;
     }
+    if (shell.wasDown('move-up')) {
+      camera.position[1] -= 1;
+    }
+    if (shell.wasDown('move-down')) {
+      camera.position[1] += 1;
+    }
     if (shell.wasDown('move-forward')) {
       camera.position[2] += 1;
     }
     if (shell.wasDown('move-back')) {
       camera.position[2] -= 1;
     }
-    //r += 1;
-    //camera.rotateY(1 * Math.PI/180);
-    //console.log(r);
   });
 
   camera.lookAt = function(eye, center, up) { console.log(eye, center, up); }; // TODO: add to basic-camera, as in orbit-camera
