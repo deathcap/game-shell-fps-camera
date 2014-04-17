073a57beefac8ebc938581a35dcc4565b63fa497
deathcap <deathcap@gmx.co.uk>
2014-04-16 18:53:35 -0700

    Load as a plugin

--- fps-camera.js
+++ fps-camera.js
@@ -2,90 +2,116 @@
 
 var glm = require('gl-matrix');
 var vec3 = glm.vec3;
-
 var createBasicCamera = require('basic-camera');
 
-var scratch0 = vec3.create();
-var y_axis = vec3.fromValues(0, 1, 0);
+module.exports = function(game, opts) {
+  return new CameraPlugin(game, opts);
+}
+module.exports.pluginInfo = {
+  clientOnly: true
+};
 
-var enableFlight; // TODO: expose
-
-var attachCamera = function(shell, opts) {
-  var camera = createBasicCamera();
+function CameraPlugin(game, opts) {
+  this.shell = game.shell;
 
   opts = opts || {};
-  enableFlight = opts.enableFlight !== undefined ? opts.enableFlight : true;
+  this.enableFlight = opts.enableFlight !== undefined ? opts.enableFlight : true;
 
-  shell.bind('move-left', 'left', 'A');
-  shell.bind('move-right', 'right', 'D');
-  shell.bind('move-forward', 'up', 'W');
-  shell.bind('move-back', 'down', 'S');
-  shell.bind('move-up', 'space');
-  shell.bind('move-down', 'shift');
+  this.camera = createBasicCamera();
+  this.camera.lookAt = function(eye, center, up) { console.log(eye, center, up); }; // TODO: add to basic-camera, as in orbit-camera (https://github.com/hughsk/basic-camera/issues/5)
+  // TODO: remove hardcoded position
+  this.camera.position[0] = -20;
+  this.camera.position[1] = -33;
+  this.camera.position[2] = -40;
 
-  var max_dpitch = Math.PI / 2;
-  var max_dyaw = Math.PI / 2;
-  var scale = 0.0002;
-  var speed = 1.0;
-  var cameraVector = vec3.create();
+  this.max_dpitch = Math.PI / 2;
+  this.max_dyaw = Math.PI / 2;
+  this.scale = 0.0002;
+  this.speed = 1.0;
+  this.cameraVector = vec3.create();
 
-  shell.on('tick', function() {
-    if (!shell.pointerLock) {
+  this.scratch0 = vec3.create();
+  this.y_axis = vec3.fromValues(0, 1, 0);
+
+  this.enable();
+}
+
+
+CameraPlugin.prototype.enable = function() {
+  this.shell.bind('move-left', 'left', 'A');
+  this.shell.bind('move-right', 'right', 'D');
+  this.shell.bind('move-forward', 'up', 'W');
+  this.shell.bind('move-back', 'down', 'S');
+  this.shell.bind('move-up', 'space');
+  this.shell.bind('move-down', 'shift');
+
+  this.shell.on('tick', this.onTick = this.tick.bind(this));
+};
+
+CameraPlugin.prototype.disable = function() {
+  this.shell.removeListener('tick', this.onTick);
+  this.shell.unbind('move-left');
+  this.shell.unbind('move-right');
+  this.shell.unbind('move-forward');
+  this.shell.unbind('move-back');
+  this.shell.unbind('move-up');
+  this.shell.unbind('move-down');
+};
+
+CameraPlugin.prototype.view = function(out) {
+  return this.camera.view(out);
+};
+
+CameraPlugin.prototype.tick = function() {
+    if (!this.shell.pointerLock) {
       return;
     }
 
     // movement relative to camera
-    camera.getCameraVector(cameraVector);
-    if (shell.wasDown('move-forward')) {
-      vec3.scaleAndAdd(camera.position, camera.position, cameraVector, speed);
+    this.camera.getCameraVector(this.cameraVector);
+    if (this.shell.wasDown('move-forward')) {
+      vec3.scaleAndAdd(this.camera.position, this.camera.position, this.cameraVector, this.speed);
     }
-    if (shell.wasDown('move-back')) {
-      vec3.scaleAndAdd(camera.position, camera.position, cameraVector, -speed);
+    if (this.shell.wasDown('move-back')) {
+      vec3.scaleAndAdd(this.camera.position, this.camera.position, this.cameraVector, -this.speed);
     }
-    if (shell.wasDown('move-right')) {
-      vec3.cross(scratch0, cameraVector, y_axis);
-      vec3.scaleAndAdd(camera.position, camera.position, scratch0, speed);
+    if (this.shell.wasDown('move-right')) {
+      vec3.cross(this.scratch0, this.cameraVector, this.y_axis);
+      vec3.scaleAndAdd(this.camera.position, this.camera.position, this.scratch0, this.speed);
     }
-    if (shell.wasDown('move-left')) {
-      vec3.cross(scratch0, cameraVector, y_axis);
-      vec3.scaleAndAdd(camera.position, camera.position, scratch0, -speed);
+    if (this.shell.wasDown('move-left')) {
+      vec3.cross(this.scratch0, this.cameraVector, this.y_axis);
+      vec3.scaleAndAdd(this.camera.position, this.camera.position, this.scratch0, -this.speed);
     }
 
     // fly straight up or down
-    if (enableFlight) {
-      if (shell.wasDown('move-up')) {
-        camera.position[1] -= 1;
+    if (this.enableFlight) {
+      if (this.shell.wasDown('move-up')) {
+        this.camera.position[1] -= 1;
       }
-      if (shell.wasDown('move-down')) {
-        camera.position[1] += 1;
+      if (this.shell.wasDown('move-down')) {
+        this.camera.position[1] += 1;
       }
     }
 
 
     // mouselook
-    var dx = shell.mouseX - shell.prevMouseX;
-    var dy = shell.mouseY - shell.prevMouseY;
-    var dt = shell.frameTime;
+    var dx = this.shell.mouseX - this.shell.prevMouseX;
+    var dy = this.shell.mouseY - this.shell.prevMouseY;
+    var dt = this.shell.frameTime;
     //console.log(dx,dy,dt);
 
-    var dpitch = dy / dt * scale;
-    var dyaw = dx / dt * scale;
+    var dpitch = dy / dt * this.scale;
+    var dyaw = dx / dt * this.scale;
 
-    if (dpitch > max_dpitch) dpitch = max_dpitch;
-    if (dpitch < -max_dpitch) dpitch = -max_dpitch;
-    if (dyaw > max_dyaw) dyaw = max_dyaw;
-    if (dyaw < -max_dyaw) dyaw = -max_dyaw;
+    if (dpitch > this.max_dpitch) dpitch = max_dpitch;
+    if (dpitch < -this.max_dpitch) dpitch = -max_dpitch;
+    if (dyaw > this.max_dyaw) dyaw = max_dyaw;
+    if (dyaw < -this.max_dyaw) dyaw = -max_dyaw;
 
     //console.log(dpitch,dyaw);
 
-    camera.rotateX(dpitch);
-    camera.rotateY(dyaw);
-  });
-
-  camera.lookAt = function(eye, center, up) { console.log(eye, center, up); }; // TODO: add to basic-camera, as in orbit-camera (https://github.com/hughsk/basic-camera/issues/5)
-
-  return camera;
+    this.camera.rotateX(dpitch);
+    this.camera.rotateY(dyaw);
 };
-
-module.exports = attachCamera;
 
